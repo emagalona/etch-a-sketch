517eaceb00b76eaf1dfd6eaeab7ffecf8207955a
Emmanuel Magalona <18748543+emagalona@users.noreply.github.com>
2024-02-17 06:22:23 -0500

    Fix naming of classes and Added User-Input Part

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const container = document.getElementById("container");
+    const container = document.getElementById("canvas");
 
     for(let i=1; i<=16; i++){
         for(let j=1; j<=16; j++){
             const content = document.createElement('div');
             content.setAttribute('id', `${i}${j}`);
-            content.textContent = `${j}`;
+            content.textContent = ``;
             container.appendChild(content);
         }
     }
