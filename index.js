2304e4a0954fd2472042379811d3ce62e355ab88
Emmanuel Magalona <18748543+emagalona@users.noreply.github.com>
2024-02-19 07:55:32 -0500

    Fixed changing of backgrounds
    fixed how functionatily of changing background color when mouse enters the div.

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ document.addEventListener("DOMContentLoaded", function () {
         for(let j=1; j<=16; j++){
             const content = document.createElement('div');
             content.setAttribute('id', `${i}${j}`);
+            content.setAttribute('class', 'content-div');
             content.textContent = ``;
             content.style.width='29.25px';
             content.style.height='29.25px';
             container.appendChild(content);
         }
     }
+
+    const canvasDivChild = document.querySelectorAll('.content-div');
+    canvasDivChild.forEach((box) => {
+        box.addEventListener('mouseover', () => {
+            box.style.background = "#008cba";
+        });
+    });
 
 });
 
@@ -38,6 +46,7 @@ changeBtn.addEventListener('click', function (){
         for (let j=1; j<= numberOfDivConverted; j++){
             const newContent = document.createElement('div');
             newContent.setAttribute('id', `${i}${j}`);
+            newContent.setAttribute('class', 'content-div');
             newContent.style.width = `${newWidth}px`;
             newContent.style.height = `${newHeight}px`;
             newContent.textContent = ``;
@@ -45,6 +54,13 @@ changeBtn.addEventListener('click', function (){
         }
     }
 
-    console.log(newDimension);
-    console.log(numberOfDiv);
-})
+    const canvasDivChild = document.querySelectorAll('.content-div');
+    canvasDivChild.forEach((box) => {
+        box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = "#008cba";
+        });
+    });
+});
+
+
+
